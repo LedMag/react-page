aa144f060ce69f981027bec7dda1f7f6edefd2b4
Magomed Aminov <ledmaggazin@gmail.com>
2022-11-15 00:07:11 +0100

    fix: error of style and filter

--- src/components/Filter/FilterStyles.ts
+++ src/components/Filter/FilterStyles.ts
@@ -2,36 +2,37 @@ import styled from 'styled-components';
 import { colors } from 'theme/colors';
 
 export const FilterBox = styled.form`
-    width: 250px;
+    width: 100%;
+    max-width: 230px;
+    padding: 10px;
     display: block;
-    background-color: red;
 `
 
 export const InputName = styled.input`
-    width: 91%;
-    margin: 5px;
+    width: 100%;
+    margin-bottom: 5px;
     padding: 5px;
 `
 export const SelectCategory = styled.select`
-    width: 96%;
-    margin: 5px;
+    width: 100%;
+    margin-bottom: 5px;
     padding: 5px;
 `
 
 export const SelectCollection = styled.select`
-    width: 96%;
-    margin: 5px;
+    width: 100%;
+    margin-bottom: 5px;
     padding: 5px;
 `
 
 export const InputMin = styled.input`
-    width: 91%;
-    margin: 5px;
+    width: 100%;
+    margin-bottom: 5px;
     padding: 5px;
 `
 
 export const InputMax = styled.input`
-    width: 91%;
-    margin: 5px;
+    width: 100%;
+    margin-bottom: 5px;
     padding: 5px;
 `
