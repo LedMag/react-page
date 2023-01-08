5e23da50e8a48eb8b6b340b1cbd5f104cfd0c626
Magomed Aminov <ledmaggazin@gmail.com>
2023-01-08 01:21:11 +0100

    feat: add auth

--- src/api/deleteProducts.ts
+++ src/api/deleteProducts.ts
@@ -1,6 +1,10 @@
 export const deleteProducts = async (id: number) => {
+    const token = document.cookie.split('=')[2];
     const res = await fetch(`${process.env.REACT_APP_API_URL}/product/delete/${id}`, {
         method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
     });
     return await res.json();
 }
