68be33ac823513fa3bde86acd38fbffeec5afa35
Vaghab <vaghabvn@gmail.com>
2023-05-01 20:21:14 +0300

    feat: add media func

--- src/pages/admin/Configurations/ConfigurationsStyle.ts
+++ src/pages/admin/Configurations/ConfigurationsStyle.ts
@@ -10,3 +10,67 @@ export const ConfigurationsContainer = styled.div`
 export const Input = styled.input`
     
 `
+export const mediaPC = (arg: any, val: any): any => {
+  return `
+    ${arg}: ${val};
+    @media (max-width: 1440px) {
+      ${arg}: ${val} * 0.75;
+    };
+    @media (max-width: 1200px) {
+      ${arg}: ${val} * 0.62;
+    };
+    @media (max-width: 1024px) {
+      ${arg}: ${val} * 0.53;
+    };
+    @media (max-width: 768px) {
+      ${arg}: ${val} * 0.4;
+    };
+  `
+};
+
+export const mediaMob = (arg: any, val: any) => {
+  return `
+    ${arg}: ${val};
+    @media (max-width: 425px) {
+      ${arg}: ${val} * 0.53;
+    }
+    @media (max-width: 390px) {
+      ${arg}: ${val} * 0.49;
+    }
+    @media (max-width: 375px) {
+      ${arg}: ${val} * 0.47;
+    }
+    @media (max-width: 320px) {
+      ${arg}: ${val} * 0.4;
+    }
+  `;
+};
+
+export const mediaAll = (arg: any, val: any): any => {
+  return `
+    ${arg}: ${val};
+    @media (max-width: 1440px) {
+      ${arg}: ${val} * 0.75;
+    }
+    @media (max-width: 1200px) {
+      ${arg}: ${val} * 0.62;
+    }
+    @media (max-width: 1024px) {
+      ${arg}: ${val} * 0.53;
+    }
+    @media (max-width: 768px) {
+      ${arg}: ${val} * 0.4;
+    }
+    @media (max-width: 425px) {
+      ${arg}: ${val} * 0.22;
+    }
+    @media (max-width: 390px) {
+      ${arg}: ${val} * 0.2;
+    }
+    @media (max-width: 375px) {
+      ${arg}: ${val} * 0.19;
+    }
+    @media (max-width: 320px) {
+      ${arg}: ${val} * 0.17;
+    }`
+};
